d0faf3d988494f5e1c702546a3a3e874c88db753
Joe O'Brien <joepu@ODEON1144L.odeon.ad>
2021-07-12 21:16:57 +0100

    final

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -5,16 +5,18 @@ import "../../App.css";
 const CharacterGrid = (props) => {
   return (
     <div className="characterGrid">
-      {props.characterList.map((character) => (
-        <div className="" key={`${character.id}+${character.name}`}>
-          <SingleCharacter
-            key={character.id}
-            name={character.name}
-            image={character.img}
-            portrayed={character.portrayed}
-          />
-        </div>
-      ))}
+      {props.characterList
+        .filter((char) => char.char_id <= 10)
+        .map((character) => (
+          <div className="" key={`${character.id}+${character.name}`}>
+            <SingleCharacter
+              key={character.id}
+              name={character.name}
+              image={character.img}
+              portrayed={character.portrayed}
+            />
+          </div>
+        ))}
     </div>
   );
 };
